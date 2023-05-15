1fb527d097ec2b5afde707f9eef542df3f84bb23
Dizrtt <demonic2003@bk.ru>
2023-05-15 20:34:39 +0200

    "Battle repository copied."

---
+++ Assasin.java
@@ -0,0 +1,11 @@
+public class Assasin extends Warrior<Knife>{
+
+    public Assasin(String name, Knife knife, int healthPoint) {
+        super(name, knife, healthPoint);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Assasin %s", super.toString());
+    }
+}
